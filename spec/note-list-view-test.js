7145fda82ddac95d23b98829e963bf1cbfb5d643
Jack <92jackhenderson@gmail.com>
2017-02-14 16:35:49 +0000

    note list view

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -2,6 +2,7 @@ function testNoteListView() {
   var notelisty = new NoteList()
   notelisty.add('Favourite Food: Gaucho Sandwich')
   notelisty.add('Favourite Drink: Fizzed Pop')
+  
   var notelistview = new NoteListView(notelisty)
 
   if(notelistview.returnHtml() !== '<ul><li><div>Favourite Food: Gaucho Sandwich</div></li><li><div>Favourite Drink: Fizzed Pop</div></li></ul>'){
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -4,12 +4,15 @@
     this.NoteList = noteList
   }
 
-  NoteListView.prototype.returnHTML = function() {
-  var listItems = ''
-  this.noteList.notes.forEach(function(i) {
-    listItems += '<li><div>' + i.content() + '</div></li>'
-  })
+  NoteListView.prototype.returnHtml = function() {
+    var listItems = ''
+      this.NoteList.reallist.forEach(function(i) {
+      listItems += '<li><div>' + i.read() + '</div></li>'
+    })
   return '<ul>' + listItems + '</ul>';
 };
+
+exports.NoteListView = NoteListView;
+exports.NoteListView.returnHtml = NoteListView.returnHtml;
 
 })(this);
