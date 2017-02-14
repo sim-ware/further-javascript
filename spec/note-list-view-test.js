69931798465d5fdd3f34a69164ce6133faeea5f8
Samir Gossain <samir.gossain@gmail.com>
2017-02-14 15:39:59 +0000

    TDD A NoteListView; Setup Failing Tests

---
+++ spec/note-list-view-test.js
@@ -0,0 +1,15 @@
+function testNoteListView() {
+  var notelisty = new NoteList()
+  notelisty.add('Favourite Food: Gaucho Sandwich')
+  notelisty.add('Favourite Drink: Fizzed Pop')
+  var notelistview = new NoteListView(notelisty)
+
+  if(notelistview.returnHtml() !== '<ul><li><div>Favourite Food: Gaucho Sandwich</div></li><li><div>Favourite Drink: Fizzed Pop</div></li></ul>'){
+    throw new Error("NoteListView Fails.")
+  } else {
+    console.log('NoteListView Works')
+  }
+
+};
+
+testNoteListView();
