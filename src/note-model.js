b844d54d30d43e9b478607d9890674a471c3a5fd
Samir Gossain <samir.gossain@gmail.com>
2017-02-16 15:15:50 +0000

    Challenge Step 9.1 - Note Model given Unique ID

--- src/note-model.js
+++ src/note-model.js
@@ -1,8 +1,18 @@
-(function(exports) {
 
+(function(exports) {
+  var id = 0
   var Note = function(string) {
     this.text = string;
   }
+
+  function generateId() { return id++ };
+
+  Note.prototype.id = function() {
+    var newId = generateId();
+
+    this.id = function() { return newId };
+    return newId;
+  };
 
   Note.prototype.read = function() {
   return this.text;
